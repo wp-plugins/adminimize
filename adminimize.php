23c86bfc0ff3a57bd3ac3746a836dedf45476591
Bueltge <Bueltge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-18 13:16:32 +0000

    Bugfix in german language file
    git-svn-id: https://plugins.svn.wordpress.org/adminimize/trunk@127039 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adminimize.php
+++ adminimize.php
@@ -5,9 +5,9 @@ Plugin URI: http://bueltge.de/wordpress-admin-theme-adminimize/674/
 Description: Visually compresses the administratrive meta-boxes so that more admin page content can be initially seen. The plugin that lets you hide 'unnecessary' items from the WordPress administration menu, for alle roles of your install. You can also hide post meta controls on the edit-area to simplify the interface. It is possible to simplify the admin in different for all roles.
 Author: Frank B&uuml;ltge
 Author URI: http://bueltge.de/
-Version: 1.6.7
+Version: 1.6.8
 License: GPL
-Last Update: 11.06.2009 11:12:38
+Last Update: 18.06.2009 15:14:11
 */
 
 /**
