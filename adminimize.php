9d54d07e5defba764db11e02d6982dd4f3ce5cb6
Bueltge <Bueltge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-22 08:29:46 +0000

    add romanian language files
    git-svn-id: https://plugins.svn.wordpress.org/adminimize/trunk@522102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adminimize.php
+++ adminimize.php
@@ -12,7 +12,7 @@
  * Description: Visually compresses the administratrive meta-boxes so that more admin page content can be initially seen. The plugin that lets you hide 'unnecessary' items from the WordPress administration menu, for alle roles of your install. You can also hide post meta controls on the edit-area to simplify the interface. It is possible to simplify the admin in different for all roles.
  * Author:      Frank B&uuml;ltge
  * Author URI:  http://bueltge.de/
- * Version:     1.7.26
+ * Version:     1.7.27
  * License:     GPLv3
  */
 
@@ -534,7 +534,7 @@ function _mw_adminimize_remove_dashboard() {
 			}
 		}
 		
-		if ( $redirect ) {
+		if ( isset($redirect) && $redirect ) {
 			$_mw_adminimize_db_redirect = _mw_adminimize_get_option_value( '_mw_adminimize_db_redirect' );
 			$_mw_adminimize_db_redirect_admin_url = get_option( 'siteurl' ) . '/wp-admin/';
 			switch ( $_mw_adminimize_db_redirect) {
